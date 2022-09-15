de84fb86141210ebd2f3b9899c3f55fdf0abbc58
WindHide <WindHide520@gmail.com>
2022-09-15 11:16:39 +0800

    [A] 网关配置修改

--- gateway/src/main/java/com/ankang/drugs/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/ankang/drugs/gateway/GatewayApplication.java
@@ -2,8 +2,9 @@ package com.ankang.drugs.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class,args);
