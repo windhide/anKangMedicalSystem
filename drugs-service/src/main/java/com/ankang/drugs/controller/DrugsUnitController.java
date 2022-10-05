6e33365283ff0d36c14e70bec6bc2a3998f198aa
WindHide <WindHide520@gmail.com>
2022-10-05 20:19:27 +0800

    [A] DrugsUnit的业务逻辑完善

--- drugs-service/src/main/java/com/ankang/drugs/controller/DrugsUnitController.java
+++ drugs-service/src/main/java/com/ankang/drugs/controller/DrugsUnitController.java
@@ -5,6 +5,7 @@ import com.ankang.pojo.drugsService.Drugs;
 import com.ankang.pojo.drugsService.DrugsUnit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,18 +28,18 @@ public class DrugsUnitController {
         return drugsUnitService.getById(drugsUnitId);
     }
 
-    @RequestMapping("update/{drugsUnit}")
-    public boolean updateDrugsById(@PathVariable("drugsUnit") DrugsUnit drugsUnit) {
+    @RequestMapping("update")
+    public boolean updateDrugsById(@RequestBody DrugsUnit drugsUnit) {
         return drugsUnitService.updateById(drugsUnit);
     }
 
-    @RequestMapping("remove/{drugsUnitId}")
-    public boolean deleteDrugsById(@PathVariable("drugsUnitId") Integer drugsUnitId) {
-        return drugsUnitService.removeById(drugsUnitId);
+    @RequestMapping("remove")
+    public boolean deleteDrugsById(@RequestBody DrugsUnit drugsUnit) {
+        return drugsUnitService.removeById(drugsUnit.getDrugsUnitId());
     }
 
-    @RequestMapping("insert/{drugsUnit}")
-    public boolean insertDrugs(@PathVariable("drugsUnit") DrugsUnit drugsUnit) {
+    @RequestMapping("insert")
+    public boolean insertDrugs(@RequestBody DrugsUnit drugsUnit) {
         return drugsUnitService.save(drugsUnit);
     }
 
