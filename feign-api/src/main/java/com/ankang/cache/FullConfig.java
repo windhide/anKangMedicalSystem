7cfcc834b11d7cb8d5c74d1170a5773998557129
WindHide <WindHide520@gmail.com>
2022-10-13 17:55:32 +0800

    [A] User-service中redis引入

---
+++ feign-api/src/main/java/com/ankang/cache/FullConfig.java
@@ -0,0 +1,7 @@
+package com.ankang.cache;
+
+public class FullConfig {
+
+    public static Long timeout = 600000L;
+
+}
--- user-service/src/main/java/com/ankang/user/controller/UserController.java
+++ user-service/src/main/java/com/ankang/user/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.ankang.user.controller;
 
+import com.alibaba.fastjson2.JSON;
+import com.ankang.cache.FullConfig;
 import com.ankang.pojo.userService.User;
 import com.ankang.pojo.userService.UserLevelType;
 import com.ankang.user.service.UserLevelTypeService;
 import com.ankang.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,25 +15,30 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("user")
 public class UserController {
+
+    String redisKey = "user";
     @Autowired
     UserService userService;
-
+    @Autowired
+    RedisTemplate redisTemplate;
     @Autowired
     UserLevelTypeService userLevelTypeService;
+
 
     Map<Integer, UserLevelType> userLevelTypeMap;
 
     @RequestMapping("select/list")
-    public List<User> queryUserForList() {
-        List<User> userList = userService.list();
-        userLevelTypeInit();
-        userList.replaceAll(this::userLevelTypeInit);
-        return userList;
+    public Object queryUserForList() {
+        if (Objects.equals(redisTemplate.opsForValue().get(redisKey), "")) {
+            cacheReload();
+        }
+        return redisTemplate.opsForValue().get(redisKey);
     }
 
     @RequestMapping("select/{userId}")
@@ -53,6 +61,13 @@ public class UserController {
         return userService.save(user);
     }
 
+    public void cacheReload() {
+        List<User> userList = userService.list();
+        userLevelTypeInit();
+        userList.replaceAll(this::userLevelTypeInit);
+        redisTemplate.opsForValue().set(redisKey, JSON.toJSON(userList), FullConfig.timeout);
+    }
+
     /**
      * 载入用户等级
      */
@@ -68,9 +83,7 @@ public class UserController {
      */
     public User userLevelTypeInit(User user) {
         UserLevelType tempUserLevelType = userLevelTypeMap.get(user.getUserLevelTypeId());
-
         user.setUserLevelType(tempUserLevelType);
-
         return user;
     }
 
