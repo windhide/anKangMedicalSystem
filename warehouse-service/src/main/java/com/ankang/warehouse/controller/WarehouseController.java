c948999c8e9e871d2e8df2e905824d8442170c2b
WindHide <WindHide520@gmail.com>
2022-10-13 22:13:17 +0800

    [A] warehouse-service引入redis

--- warehouse-service/src/main/java/com/ankang/warehouse/controller/WarehouseController.java
+++ warehouse-service/src/main/java/com/ankang/warehouse/controller/WarehouseController.java
@@ -1,14 +1,16 @@
 package com.ankang.warehouse.controller;
 
+import com.alibaba.fastjson2.JSON;
+import com.ankang.cache.FullConfig;
 import com.ankang.clients.DrugsClient;
 import com.ankang.clients.PharmacyClient;
 import com.ankang.clients.StaffClient;
 import com.ankang.pojo.drugsService.Drugs;
 import com.ankang.pojo.staffService.Pharmacy;
-import com.ankang.pojo.staffService.Staff;
 import com.ankang.pojo.warehouseService.Warehouse;
 import com.ankang.warehouse.service.WarehouseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,11 +18,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("warehouse")
 public class WarehouseController {
+
+    String redisKey = "warehouse";
+
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
     @Autowired
     WarehouseService warehouseService;
 
@@ -38,11 +47,12 @@ public class WarehouseController {
     Map<Integer, Pharmacy> pharmacyMap;
 
     @RequestMapping("select/list")
-    public List<Warehouse> queryWarehouseForList() {
-        List<Warehouse> warehouseList = warehouseService.list();
-        staffAndDrugsAndPharmacyInit();
-        warehouseList.replaceAll(this::staffAndDrugsAndPharmacyInit);
-        return warehouseList;
+    public Object queryWarehouseForList() {
+        Object cacheData = stringRedisTemplate.opsForValue().get(redisKey);
+        if (Objects.equals(cacheData, "") || cacheData == null) {
+            return cacheReload();
+        }
+        return cacheData;
     }
 
     @RequestMapping("select/{warehouseId}")
@@ -52,24 +62,44 @@ public class WarehouseController {
 
     @RequestMapping("update")
     public boolean updateWarehouseById(@RequestBody Warehouse warehouse) {
-        return warehouseService.updateById(warehouse);
+        if (warehouseService.updateById(warehouse)) {
+            cacheReload();
+            return true;
+        }
+        return false;
     }
 
     @RequestMapping("remove")
     public boolean deleteWarehouseById(@RequestBody Warehouse warehouse) {
-        return warehouseService.removeById(warehouse.getWarehouseId());
+        if (warehouseService.removeById(warehouse.getWarehouseId())) {
+            cacheReload();
+            return true;
+        }
+        return false;
     }
 
     @RequestMapping("insert")
     public boolean insertWarehouse(@RequestBody Warehouse warehouse) {
-        return warehouseService.save(warehouse);
+        if (warehouseService.save(warehouse)) {
+            cacheReload();
+            return true;
+        }
+        return false;
+    }
+
+    public Object cacheReload() {
+        List<Warehouse> warehouseList = warehouseService.list();
+        staffAndDrugsAndPharmacyInit();
+        warehouseList.replaceAll(this::staffAndDrugsAndPharmacyInit);
+        stringRedisTemplate.opsForValue().set(redisKey, JSON.toJSON(warehouseList).toString(), FullConfig.timeout, TimeUnit.SECONDS);
+        return warehouseList;
     }
 
     /**
      * 载入员工、药品、药房信息
      */
     public void staffAndDrugsAndPharmacyInit() {
-        drugsMap = drugsClient.queryDrugsForList().stream().collect(Collectors.toMap(Drugs::getDrugsId,drugs -> drugs));
+        drugsMap = drugsClient.queryDrugsForList().stream().collect(Collectors.toMap(Drugs::getDrugsId, drugs -> drugs));
         pharmacyMap = pharmacyClient.queryPharmacyForList().stream().collect(Collectors.toMap(Pharmacy::getPharmacyId, pharmacy -> pharmacy));
     }
 
