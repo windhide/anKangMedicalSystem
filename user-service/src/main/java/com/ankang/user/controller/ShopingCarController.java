2802e7728f016026fc4b10ff6290aff3af764417
WindHide <WindHide520@gmail.com>
2022-11-09 11:30:44 +0800

    [A] 完成了购物车的添加和查询接口的逻辑编写

--- user-service/src/main/java/com/ankang/user/controller/ShopingCarController.java
+++ user-service/src/main/java/com/ankang/user/controller/ShopingCarController.java
@@ -1,13 +1,17 @@
 package com.ankang.user.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.ankang.pojo.userService.ShopingCar;
+import org.apache.commons.lang.StringEscapeUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -18,16 +22,41 @@ public class ShopingCarController {
     StringRedisTemplate stringRedisTemplate;
 
     @RequestMapping("select")
-    public String selectShopingCar(@RequestBody String userId){
-        return null;
+    public String selectShopingCar(@RequestBody String userKey){
+        userKey = userKey.replaceAll("\\{\"userKey\":\"","").replaceAll("\"\\}","");
+        System.out.println("userKey ==========+>"+ userKey);
+        String redisData = stringRedisTemplate.opsForValue().get(userKey);
+        System.out.println("redisData =======>" + redisData);
+        return redisData == null || redisData.equals("") ? "" : redisData;
     }
 
     @RequestMapping("insert")
     public boolean addShopingCar(@RequestBody Map<String, Object> dataMap){
-        System.out.println("keys=======>"+ dataMap.get("shopingCar").toString());
-        System.out.println("keys=======>"+ dataMap.get("userKey"));
+        String userKey = StringEscapeUtils.unescapeHtml(dataMap.get("userKey").toString());
+        String redisData = stringRedisTemplate.opsForValue().get(userKey);
+        boolean state = false;
+        ShopingCar inputShopingCar = (ShopingCar) JSON.parseObject(JSON.toJSONString(dataMap.get("shopingCar")), ShopingCar.class);
+        if(redisData == null || redisData.equals("")){
+            try {
+                List<ShopingCar> list = new ArrayList<>();
+                list.add(inputShopingCar);
+                stringRedisTemplate.opsForValue().set(userKey,JSON.toJSONString(list));
+                state = true;
+            }catch (Exception e){
+                state = false;
+            }
+        }else{
+            try {
+                List<ShopingCar> list = JSON.parseArray(redisData, ShopingCar.class);
+                list.add(inputShopingCar);
+                stringRedisTemplate.opsForValue().set(userKey,JSON.toJSONString(list));
+                state = true;
+            }catch (Exception e){
+                state = false;
+            }
 
-        return false;
+        }
+        return state;
     }
 
     @RequestMapping("remove")
